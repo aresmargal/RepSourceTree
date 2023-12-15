63712dec553ec2bc1f8d4f779bbbff91afb10f73
aresmargal <151525545+aresmargal@users.noreply.github.com>
2023-12-15 10:53:18 +0100

    Update GitCliente.java

--- AppGitClient/src/appgitclient/GitCliente.java
+++ AppGitClient/src/appgitclient/GitCliente.java
@@ -46,4 +46,8 @@ public class GitCliente {
     public void setLicencia(String licencia){
     this.licencia = licencia; 
     }
+    @Override 
+    public String toString(){
+        return "GtCliente{" + "cliente=" + cliente + ", version= " + version + ", sistemOper =" + sistemOper + ", licencia=" + licencia + "}";
+    }
 }
